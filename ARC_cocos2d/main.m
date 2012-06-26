cf9cbd54bc08182b0087d2325c1de2798e05ff5f
lenny0929 <lenny0929@msn.com>
2012-06-26 21:06:37 +0800

    Initial Commit

---
+++ ARC_cocos2d/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ARC_cocos2d
+//
+//  Created by Chien-Lung Chen on 6/26/12.
+//  Copyright USC 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
